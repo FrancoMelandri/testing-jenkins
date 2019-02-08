b765b7dedd1ddbc18b8894228c5f136d37794778
francomelandri <franco.melandri@gmail.com>
2019-02-08 09:09:02 +0100

    creating maven project tree structure.

---
+++ src/main/groovy/com/mycompany/app/Pipeline.groovy
@@ -0,0 +1,5 @@
+package com.mycompany.app;
+
+def call() {
+    println( "Hello World!" )
+}
---
+++ src/test/groovy/com/mycompany/app/PipelineTest.groovy
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+class PipelineTest 
+{
+    @Test
+    void shouldAnswerWithTrue()
+    {
+        assertTrue( true )
+    }
+}
